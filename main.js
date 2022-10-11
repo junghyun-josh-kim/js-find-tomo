f01efaf6eec706e9dbcceb6e302d3c123eac09ed
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2022-10-11 18:26:22 -0400

    JS: Sound

--- main.js
+++ main.js
@@ -13,9 +13,17 @@ const popUp = document.querySelector('.pop-up');
 const popUpMessage = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
 
+const tomoSound = new Audio('./sound/tomo-hit.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const ramenSound = new Audio('./sound/ramen-hit.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
+
 let started = false;
 let score = 0;
 let timer = undefined;
+
+field.addEventListener('click', onFieldClick);
 
 gameBtn.addEventListener('click', () => {
   if (started) {
@@ -23,26 +31,51 @@ gameBtn.addEventListener('click', () => {
   } else {
     startGame();
   }
-  started = !started;
 });
+
+popUpRefresh.addEventListener('click', () => {
+  startGame();
+  hidePopUp();
+});
+
 function startGame() {
+  started = true;
   initGame();
   showStopButton();
   showTimeAndScore();
   stratGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
+  started = false;
   stopGameTimer();
   hideStartButton();
   showPopUpWithText('Replay❓');
+  playSound(alertSound);
+  stopSound(bgSound);
 }
+
+function finishGame(win) {
+  started = false;
+  hideStartButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(ramenSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
+  showPopUpWithText(win ? 'YOU WON 👍' : 'YOU LOST 😅');
+}
+
 function stratGameTimer() {
   let remaingTimeSec = GAME_DURATION_SEC;
   updateTimerText(remaingTimeSec);
   timer = setInterval(() => {
     if (remaingTimeSec <= 0) {
       clearInterval(timer);
+      finishGame(TOMO_COUNT === score);
       return;
     }
     updateTimerText(--remaingTimeSec);
@@ -62,10 +95,15 @@ function showPopUpWithText(text) {
   popUp.classList.remove('pop-up--hide');
 }
 
+function hidePopUp() {
+  popUp.classList.add('pop-up--hide');
+}
+
 function showStopButton() {
-  const icon = gameBtn.querySelector('.fa-play');
+  const icon = gameBtn.querySelector('.fas');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+  gameBtn.style.visibility = 'visible';
 }
 function hideStartButton() {
   gameBtn.style.visibility = 'hidden';
@@ -77,10 +115,42 @@ function showTimeAndScore() {
 }
 
 function initGame() {
+  score = 0;
   field.innerHTML = '';
   gameScore.innerText = TOMO_COUNT;
   addItem('tomo', TOMO_COUNT, 'img/tomo.png');
   addItem('ramen', RAMEN_COUNT, 'img/ramen.png');
+}
+
+function onFieldClick(event) {
+  if (!started) {
+    return;
+  }
+  const target = event.target;
+  if (target.matches('.tomo')) {
+    target.remove();
+    score++;
+    playSound(tomoSound);
+    updateScoreBoard();
+    if (score === TOMO_COUNT) {
+      finishGame(true);
+    }
+  } else if (target.matches('.ramen')) {
+    finishGame(false);
+  }
+}
+
+function playSound(sound) {
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.currentTime = 0;
+  sound.pause();
+}
+
+function updateScoreBoard() {
+  gameScore.innerText = TOMO_COUNT - score;
 }
 
 function addItem(className, count, imgPath) {
